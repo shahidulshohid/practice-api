e6fec89b9bd3cd6b194a6610ba28483d7fe77807
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-12 18:36:42 +0600

    localStorage 3 times practice is done

--- module.36.5/cart3.js
+++ module.36.5/cart3.js
@@ -3,7 +3,10 @@ const addProduct = () => {
     const quantityField = document.getElementById('product-quantity')
     const product = productField.value 
     const quantity = quantityField.value
+    productField.value = ''
+    quantityField.value = ''
     displayProduct(product, quantity)
+    saveProductToLocalStorage(product, quantity)
 }
 
 const displayProduct = (product, quantity) => {
@@ -14,10 +17,27 @@ const displayProduct = (product, quantity) => {
 }
 
 const getStoredShoppingCart = () => {
-    let cart = {name:'shahidl', age:22}
+    let cart = {}
     const storedCart = localStorage.getItem('cart')
     if(storedCart){
-        cart = JSON.stringify(cart)
+        cart = JSON.parse(storedCart)
     }
-    console.log(cart)
+    return cart
 }
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart()
+    cart[product] = quantity
+    
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem('cart', cartStringified)
+}
+
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart()
+    for(const product in savedCart){
+        const quantity = savedCart[product]
+        displayProduct(product, quantity)
+    }
+}
+displayProductsFromLocalStorage()
