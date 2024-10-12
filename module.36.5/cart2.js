c6c1dc4fa5df04ff9b65e338c1a1f10b9bec2147
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-12 16:35:56 +0600

    localStorage practice is done

--- module.36.5/cart2.js
+++ module.36.5/cart2.js
@@ -1,47 +1,48 @@
 const addProduct = () => {
     const productField = document.getElementById('product-name')
     const quantityField = document.getElementById('product-quantity')
-    const product = productField.value
+    const product = productField.value 
     const quantity = quantityField.value
-    productField.value = ""
-    quantityField.value = ""
-    console.log(product, quantity)
+    productField.value = ''
+    quantityField.value = ''
+
     displayProduct(product, quantity)
     saveProductToLocalStorage(product, quantity)
 }
 
+// this function is displayProduct 
 const displayProduct = (product, quantity) => {
     const ul = document.getElementById('product-container')
     const li = document.createElement('li')
-    li.innerText = `${product}: ${quantity}`;
+    li.innerText = `${product} : ${quantity}`;
+
     ul.appendChild(li)
 }
 
 const getStoredShoppingCart = () => {
     let cart = {}
-    const storedCard = localStorage.getItem('cart')
-    if(storedCard){
-        cart = JSON.parse(storedCard)
+    const storedCart = localStorage.getItem('cart')
+    
+    if(storedCart){
+        cart = JSON.parse(storedCart)
     }
     return cart
 }
 
 const saveProductToLocalStorage = (product, quantity) => {
     const cart = getStoredShoppingCart()
-    cart[product] = quantity//==================
-    console.log(cart)
+    cart[product] = quantity;
     const cartStringified = JSON.stringify(cart)
-    console.log(cartStringified)
-    localStorage.setItem('cart', cartStringified)
 
+    localStorage.setItem('cart', cartStringified)
+    console.log(cartStringified)
 }
 
 const displayProductsFromLocalStorage = () => {
     const savedCart = getStoredShoppingCart()
-    console.log(savedCart)
+    // console.log(savedCart)
     for(const product in savedCart){
         const quantity = savedCart[product]
-        console.log(product, quantity)
         displayProduct(product, quantity)
     }
 }
